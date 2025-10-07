b20a651ce7b4b39ec07acc255fae440865323c2e
Сергей Оберемок Ип-514 <seregei.oberemok@yandex.ru>
2025-10-07 19:08:38 +0700

    Исправлено приветствие

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@ int main(int argc, char** argv) {
     char* str = (char*)malloc(sizeof(char) * SIZE);
     printf("Введите своё имя:\n");
     scanf("%s", str);
-    printf("Привет, %s", str);
+    puts("Привет");
+    puts(str);
     return 0;
 }
